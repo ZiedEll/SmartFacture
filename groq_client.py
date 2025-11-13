7a7d27ee64ed17b7793a959f405564a7b4687f25
ZiedEll <104266001+ZiedEll@users.noreply.github.com>
2025-11-13 14:02:33 +0100

    Update groq_client.py

--- groq_client.py
+++ groq_client.py
@@ -2,5 +2,5 @@
 from groq import Groq
 
 def get_groq_client():
-    api_key = "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"
+    api_key = "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"
     return Groq(api_key=api_key)
